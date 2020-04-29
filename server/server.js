4044080cb0f33fbab14b496252d350f9ad169b26
Teo <43215755+teokigi@users.noreply.github.com>
2020-04-29 19:08:15 +0100

    +1CatData,Duplicated createRouter for it
    Added cats data to server, duplicated createRouter for use with cats data

--- server/server.js
+++ server/server.js
@@ -12,12 +12,22 @@ const dogs = [
         { name: "Monty", breed: "Maine Coon" }
 ];
 
+const cats = [
+    { name: "Tiddles", breed: "Siberian" },
+    { name: "Patch", breed: "Bengal" },
+    { name: "Socks", breed: "British Shorthair" },
+    { name: "Mr Bigglesworth", breed: "Persian" },
+    { name: "Monty", breed: "Maine Coon" }
+];
+
 app.use(cors());
 app.use(bodyParser.json());
 
 const dogsRouter = createRouter(dogs);
+const catsRouter = createRouter(cats);
 
 app.use('/api/dogs/', dogsRouter)
+app.use('/api/cats/', catsRouter)
 
 app.listen(3000,function(){
     console.log(`App running on port ${ this.address().port}`);
